ea2f8a2fff16790eaba331e0dce0be5ae39800cc
pranayapril30 <70978034+pranayapril30@users.noreply.github.com>
2021-09-04 13:35:02 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA5qs4uHJlx04D_kWXn9IG76nrfajY0zMk",
+  authDomain: "storytelling-app-e29db.firebaseapp.com",
+  projectId: "storytelling-app-e29db",
+  storageBucket: "storytelling-app-e29db.appspot.com",
+  messagingSenderId: "244825938697",
+  appId: "1:244825938697:web:f5db190cee3e967a5cd1d8",
+  measurementId: "G-LQJJ2VP2FG"
+};
